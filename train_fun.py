69104ee63965c35db8f7e36ea3c572d255791f24
Farid <44614215+FaridAF276@users.noreply.github.com>
2022-07-08 17:12:38 +0200

    self.optimizer

--- train_fun.py
+++ train_fun.py
@@ -59,7 +59,7 @@ class TrainUtils:
             total_num += data_loader.batch_size
             total_loss += loss.item() * data_loader.batch_size
             train_bar.set_description(
-                'Train Epoch: [{}/{}], lr: {:.6f}, Loss: {:.4f}'.format(epoch, args.epochs, train_optimizer.param_groups[0]['lr'],
+                'Train Epoch: [{}/{}], lr: {:.6f}, Loss: {:.4f}'.format(epoch, args.epochs, self.optimizer.param_groups[0]['lr'],
                                                                         total_loss / total_num))
             
         return total_loss / total_num
