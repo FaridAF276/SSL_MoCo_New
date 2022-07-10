4664e1b586c42ca326e40aafc1bee6dd45ac9d20
Farid <44614215+FaridAF276@users.noreply.github.com>
2022-07-10 22:06:18 +0200

    Update linear_eval.py

--- linear_eval.py
+++ linear_eval.py
@@ -78,7 +78,6 @@ def get_cifar10_data_loaders(download, shuffle=False, batch_size=args.batch_size
     return train_loader, test_loader
 def get_folder_data_loaders(shuffle=True, batch_size=args.batch_size, root_folder=''):
     folder_transform=transforms.Compose([
-        transforms.Resize((1024,1024)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
     ])
