5f7daae1106d9a56c2b8b352098f95f20feeba84
Farid <farid-f32@msn.com>
2022-07-20 17:07:58 +0200

    splitting files

---
+++ quickstart_cifar10.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+time python dataset_preparation.py \
+--dataset_dir cifar10 \
+--percentage 0.2 \
+--split_train_test
+#To start from here type this command : 
+# tail -n +17 quickstart_chestxray.sh | bash
+
+#Create directories for train et eval models
+mkdir MoCo_train_checkpoints && \
+mkdir MoCo_eval_checkpoints
+python -c "import torch; import torchvision; print('\n Torch version:\t', torch.__version__, '\n Torchvision version:\t', torchvision.__version__)"
+# #Launch training process
+time python pre_train.py \
+--epochs 200 \
+--batch_size 16 \
+--lr 0.6 \
+--results-dir "MoCo_train_checkpoints/" \
+--dataset "folder" \
+--root_folder "pretext" \
+--cos \
+--knn-k 4000 \
+--bn-splits 1
+touch MoCo_train_checkpoints/linear_eval.log
+zip -r chest_pretext.zip MoCo_train_checkpoints
+./gdrive upload chest_pretext.zip
+time python linear_eval.py \
+--epochs 200 \
+--batch_size 16 \
+--lr 0.6 \
+--model-dir "MoCo_train_checkpoints/" \
+--dataset-ft "folder" \
+--results_dir "MoCo_eval_checkpoints/" \
+--root_folder "downstream" \
+--cos \
+--num_classes 3 \
+-pt-ssl
+
+# #Zip the result and upload them to drive
+
+zip -r chest_dowstr.zip MoCo_eval_checkpoints
+./gdrive upload chest_dowstr.zip
+./vast stop instance ${VAST_CONTAINERLABEL:2}
