b60de7e0c4fdc13797007306daa37f46503b6cfe
Farid <farid-f32@msn.com>
2022-07-23 20:47:41 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -9,7 +9,7 @@ cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VA
 #shell script
 # tail -n +17 quickstart_chestxray.sh | bash
 apt update -y
-pip install pandas matplotlib tensorboard Pillow split-folders gdown
+pip install pandas matplotlib tensorboard Pillow split-folders gdown pandas
 #Download and connect with gdrive
 wget -nc https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 tar -xvf gdrive_2.1.1_linux_386.tar.gz
