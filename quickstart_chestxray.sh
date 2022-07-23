9a159353d01ba88adeede2ae49305fbfc1ee7ad7
Farid <farid-f32@msn.com>
2022-07-23 16:23:19 +0200

    comment

--- quickstart_chestxray.sh
+++ quickstart_chestxray.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-'''
-cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key; && \
-wget -nc https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast; && \
-./vast start instance ${VAST_CONTAINERLABEL:2} && \
-bash -e SSL_MoCo_New/quickstart_chestrxray.sh && \
-./vast stop instance ${VAST_CONTAINERLABEL:2}
-'''
+
+# cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key; && \
+# wget -nc https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast; && \
+# ./vast start instance ${VAST_CONTAINERLABEL:2} && \
+# bash -e SSL_MoCo_New/quickstart_chestrxray.sh && \
+# ./vast stop instance ${VAST_CONTAINERLABEL:2}
+
 cd SSL_MoCo_New
 wget -nc 
 #Around 5 GB used
--- quickstart_imagenet.sh
+++ quickstart_imagenet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-'''
-cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key; && \
-wget -nc https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast; && \
-./vast start instance ${VAST_CONTAINERLABEL:2} && \
-bash -e SSL_MoCo_New/quickstart_imagenet.sh && \
-./vast stop instance ${VAST_CONTAINERLABEL:2}
-'''
+
+# cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key; && \
+# wget -nc https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast; && \
+# ./vast start instance ${VAST_CONTAINERLABEL:2} && \
+# bash -e SSL_MoCo_New/quickstart_imagenet.sh && \
+# ./vast stop instance ${VAST_CONTAINERLABEL:2}
+
 #shell script
 #Download ImageNet dataset
 gdown --fuzzy https://drive.google.com/file/d/1NeBMqfrgLPJcb6_w9-2QZ7ZgYeSzG__u/view?usp=sharing && unzip tiny_imagenet_200.zip
