eb99d94cd8aaa16e22de9f54b3685e76991a3ba0
Farid <farid-f32@msn.com>
2022-07-24 14:37:29 +0200

    earlstp

--- train_fun.py
+++ train_fun.py
@@ -24,7 +24,7 @@ class TrainUtils:
         self.test_loader = test_loader
         self.writer = SummaryWriter() #auto creates runs/Apr07_18-01-17_d1007 DIR
         self.path = os.path.join(self.args.results_dir)
-        self.EarlStop = EarlyStopping(patience=5, verbose=True)
+        self.EarlStop = EarlyStopping
         if not os.path.exists(self.path):
             os.makedirs(self.path)
 
@@ -163,7 +163,7 @@ class TrainUtils:
         last_loss=0
         epoch_start = 1
         self.n_iter= 0
-
+        self.EarlStop(patience=5, verbose=True)
         if(self.args.resume is ''):
             logging.info(f"Start MoCo training for {self.args.epochs} epochs.")
 
@@ -171,8 +171,8 @@ class TrainUtils:
             train_loss = self.train_one_epoch(self.model, self.train_loader, self.optimizer, epoch, self.args)
             last_loss=train_loss
             logging.info("Epoch: {}\ttrain_loss: {:.3f}\tAcc@1: {:.3f}\tAcc@5: {:.3f}".format(epoch,train_loss,self.top1[0], self.top5[0]))
-            torch.save({'epoch': epoch, 'state_dict': self.model.state_dict(), 'optimizer' : self.optimizer.state_dict(),}, os.path.join(self.args.results_dir,'model.pth'))
-            self.EarlStop(train_loss, self.model)
+            # torch.save({'epoch': epoch, 'state_dict': self.model.state_dict(), 'optimizer' : self.optimizer.state_dict(),}, os.path.join(self.args.results_dir,'model.pth'))
+            self.EarlStop(train_loss, self.model, self.optimizer, self.args, epoch)
             if self.EarlStop.early_stop:
                 print("Model not improving, stopping training")
                 break;
