097f11c8bc2bcf424353c2309dcc765aa724a997
Farid <farid-f32@msn.com>
2022-07-28 14:08:33 +0200

    Update moco_dataset_generator.py

--- moco_dataset_generator.py
+++ moco_dataset_generator.py
@@ -46,7 +46,7 @@ class MocoDatasetGenerator:
                     transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
                 ], p=0.8),
                 transforms.RandomGrayscale(p=0.2),
-                transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5), #Pas partinent pour Chestxray
+                # transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5), #Pas partinent pour Chestxray
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 normalize
