1cd4e79b2d61cebf3b20fab2759162602e830c55
Farid <farid-f32@msn.com>
2022-07-29 16:44:36 +0200

    Update fine_tune.sh

--- fine_tune.sh
+++ fine_tune.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#cd
+# wget -nc https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast; \
+# cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key; \
+# ./vast start instance ${VAST_CONTAINERLABEL:2} && \
+# bash -e SSL_MoCo_New/quickstart_cifar10.sh && \
+# ./vast stop instance ${VAST_CONTAINERLABEL:2}
 cd SSL_MoCo_New
 ##Download pretrained model
 gdown --fuzzy -O temp.zip https://drive.google.com/file/d/1JTRGAKh9vAcp6ZBmdnJ1riWq03pXgnKV/view?usp=sharing
