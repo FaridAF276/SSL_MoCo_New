fc8862e3be5274cf8e2f0cc1b7d96870aaca3e24
Farid <farid-f32@msn.com>
2022-08-06 20:37:32 +0200

    Update fine_tune_chest.sh

--- fine_tune_scripts/fine_tune_chest.sh
+++ fine_tune_scripts/fine_tune_chest.sh
@@ -22,7 +22,7 @@ mkdir -p MoCo_eval_checkpoints
 time python linear_eval.py \
 --epochs 500 \
 --batch_size 256 \
---lr 0.01 \
+--lr 1 \
 --model-dir "MoCo_train_checkpoints/" \
 --dataset-ft "folder" \
 --results_dir "MoCo_eval_checkpoints/" \
